0e833459a67517074ad876e27e92c27632d3b4db
Leonardo Oliveros C <skykos69@gmail.com>
2021-11-20 14:18:02 -0500

    Subida parcial del ms de usuario en python Leonardo

---
+++ Usuario/models.py
@@ -0,0 +1,18 @@
+# models.py in the users Django app
+from django.db import models
+from django.contrib.auth.models import AbstractUser
+
+
+GENDER_SELECTION = [
+    ('M', 'Male'),
+    ('F', 'Female'),
+    ('NS', 'Not Specified'),
+]
+
+
+class CustomUser(AbstractUser):
+    # We don't need to define the email attribute because is inherited from AbstractUser
+    gender = models.CharField(max_length=20, choices=GENDER_SELECTION)
+    phone_number = models.CharField(max_length=30)
+    first_name = models.CharField( max_length=40, verbose_name='first name')
+    last_name = models.CharField(max_length=40, verbose_name='last name')
---
+++ Usuario/serializers.py
@@ -0,0 +1,27 @@
+# serializers.py in the users Django app
+from django.db import transaction
+from rest_framework import serializers
+from rest_auth.registration.serializers import RegisterSerializer
+
+from .models import GENDER_SELECTION
+
+
+class CustomRegisterSerializer(RegisterSerializer):
+    gender = serializers.ChoiceField(choices=GENDER_SELECTION)
+    phone_number = serializers.CharField(max_length=30)
+    first_name = serializers.CharField(max_length=40)
+    last_name = serializers.CharField(max_length=40)
+
+    
+
+
+    # Define transaction.atomic to rollback the save operation in case of error
+    @transaction.atomic
+    def save(self, request):
+        user = super().save(request)
+        user.gender = self.data.get('gender')
+        user.phone_number = self.data.get('phone_number')
+        user.first_name = self.data.get('first_name')
+        user.last_name = self.data.get('last_name')
+        user.save()
+        return user

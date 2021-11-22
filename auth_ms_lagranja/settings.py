158881db2e313fee7bfa9e654b5442aa2d542a9e
Leonardo Oliveros C <skykos69@gmail.com>
2021-11-21 19:53:31 -0500

    probando subir el restfull

--- auth_ms_lagranja/settings.py
+++ auth_ms_lagranja/settings.py
@@ -18,7 +18,7 @@ from pathlib import Path
 BASE_DIR = Path(__file__).resolve().parent.parent
 
 
-# Quick-start development settings - unsuitable for production
+# Quick-start development settings - unsuitable for production.
 # See https://docs.djangoproject.com/en/3.2/howto/deployment/checklist/
 
 # SECURITY WARNING: keep the secret key used in production secret!
